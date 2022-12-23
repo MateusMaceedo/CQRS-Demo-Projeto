9a5b9f753c3ee884a712831c3803f45d598e1a13
Mateus Macedo <mateusouza2014@live.com>
2022-12-22 22:11:21 -0300

    feat: Estrutura inicial do projeto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# api-cartao-virtual
+# AulaCQRS
+
+https://www.youtube.com/watch?v=hlum-rdeWoM
--- Shared/Commands/ICommandResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.Commands
-{
-    public interface ICommandResult
-    {
-        public bool Success { get; set; }
-        public string Message { get; set; }
-        public object Data { get; set; }
-    }
-}
